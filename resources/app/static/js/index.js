cc77fd8e72a8c77e8a76e13b8318da6a72bab90a
CanobbioE <canobbio.e@gmail.com>
2018-10-05 21:13:28 +0200

    Add event listener

--- resources/app/static/js/index.js
+++ resources/app/static/js/index.js
@@ -4,5 +4,25 @@ let index = {
 		asticode.loader.init();
 		asticode.modaler.init();
 		asticode.notifier.init();
+
+		document.addEventListener('astilectron-ready', function() {
+			index.listen();
+			index.showmain();
+		})
+	},
+	addMeal: function() {
+		// TODO add meal
+	},
+	newFood: function() {
+		// TODO new food item
+	},
+	listen: function() {
+		astilectron.onMessage(function(message) {
+			switch (message.name) {
+				case "event.name":
+					// TODO
+					break;
+			}
+		});
 	}
 };
