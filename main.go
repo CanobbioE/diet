f2321f2edd44b29b4971acd5c3a1ce0ee0da8ad1
CanobbioE <canobbio.e@gmail.com>
2018-10-05 20:50:45 +0200

    Implemented GUI skeleton

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
-	"path"
+	"time"
+
+	"github.com/asticode/go-astilectron"
+	bootstrap "github.com/asticode/go-astilectron-bootstrap"
+	"github.com/asticode/go-astilog"
+	"github.com/pkg/errors"
 )
 
-const version = "v0.0"
+const (
+	version = "v0.0"
+)
 
 var (
 	showHelp    bool
 	mealToAdd   string
 	newFoodItem string
+	debug       bool
+	w           *astilectron.Window
+	AppName     string
+	BuiltAt     string
 )
 
 func usage(appName, version string) {
@@ -26,33 +37,65 @@ func usage(appName, version string) {
 	fmt.Printf("\nVersion: %s\n", version)
 }
 
-func init() {
+func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
+	switch m.Name {
+	case "event.name":
+		var s string
+		if err = json.Unmarshal(m.Payload, &s); err != nil {
+			payload = err.Error()
+			return
+		}
+	}
+	return
+}
 
-	// TODO: create db if it doesn-t exist
+func init() {
+	// TODO: create db if it doesn't exist
 	// read flags
 	flag.BoolVar(&showHelp, "h", false, "dispaly help")
-	flag.BoolVar(&showHelp, "help", false, "display help")
-	flag.StringVar(&mealToAdd, "a", mealToAdd, "comma separeted string of ingridients eaten")
-	flag.StringVar(&mealToAdd, "add", mealToAdd, "comma separeted string of ingridients eaten")
-	flag.StringVar(&newFoodItem, "n", newFoodItem, "name of the new food item and the groups it is part of (eg \"foodName 1234567\")")
-	flag.StringVar(&newFoodItem, "new", newFoodItem, "name of the new food item and the groups it is part of (eg \"foodName 1234567\")")
+	flag.BoolVar(&showHelp, "help", false, "dispaly help")
+	flag.BoolVar(&debug, "d", false, "enables debug mode")
+	flag.BoolVar(&debug, "debug", false, "enables debug mode")
 }
 
 func main() {
-	// TODO gui
-	appName := path.Base(os.Args[0])
+	// Init
 	flag.Parse()
+	astilog.FlagInit()
 
-	if showHelp == true {
-		usage(appName, version)
-	}
-
-	if mealToAdd != "" {
-		// TODO: use the controller to add the meal to the model
-		fmt.Printf("Specified %s as meal to add\n", mealToAdd)
-	}
-	if newFoodItem != "" {
-		// TODO: use the controller to add the item after parsing the flag
-		fmt.Printf("Specified %s as a new food item\n", newFoodItem)
+	// Run bootstrap
+	astilog.Debugf("Running app built at %s", BuiltAt)
+	if err := bootstrap.Run(bootstrap.Options{
+		AstilectronOptions: astilectron.Options{
+			AppName:            AppName,
+			AppIconDarwinPath:  "resources/icon.icns",
+			AppIconDefaultPath: "resources/icon.png",
+		},
+		Debug: debug,
+		MenuOptions: []*astilectron.MenuItemOptions{{
+			Label: astilectron.PtrStr("Menu"),
+		}},
+		OnWait: func(_ *astilectron.Astilectron, ws []*astilectron.Window, _ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
+			w = ws[0]
+			go func() {
+				time.Sleep(5 * time.Second)
+				if err := bootstrap.SendMessage(w, "check.out.menu", "Don't forget to check out the menu!"); err != nil {
+					astilog.Error(errors.Wrap(err, "sending check.out.menu event failed"))
+				}
+			}()
+			return nil
+		},
+		Windows: []*bootstrap.Window{{
+			Homepage: "index.html",
+			Options: &astilectron.WindowOptions{
+				BackgroundColor: astilectron.PtrStr("#333"),
+				Center:          astilectron.PtrBool(true),
+				Height:          astilectron.PtrInt(700),
+				Width:           astilectron.PtrInt(700),
+			},
+			MessageHandler: handleMessages,
+		}},
+	}); err != nil {
+		astilog.Fatal(errors.Wrap(err, "running bootstrap failed"))
 	}
 }
